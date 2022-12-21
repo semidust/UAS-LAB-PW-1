61ebe4c3612bb99c2745129fb10b110a4b06c465
semionc <x.sammythagi@gmail.com>
2022-12-21 12:53:32 +0700

    update

--- layout/header.php
+++ layout/header.php
@@ -13,13 +13,16 @@
 
   <body>
     <div class="header">  
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
+                
                 <div class="collapse navbar-collapse" id="navbarNav">
+        
                 <ul class="navbar-nav">
+                
                     <li class="nav-item">
                         <a class="nav-link" href="">HOME</a>
                     </li>
@@ -38,11 +41,14 @@
 
                     <li class="nav-item">
                         <a class="nav-link" href="">PENDAFTARAN</a>
-                    </li>         
+                    </li> 
+                    
                 </ul>
                 </div>
-                <span style="float: right; color: gray">
-                        <a class="nav-link" href=""><i class="fa-solid fa-user"></i> Login</a>
+                <span class="ms-auto">
+                    <a class="nav-link" href="">
+                        <i class="fa-solid fa-user" style="color: white"></i>
+                    </a>
                 </span>
             </div>
         </nav> 
