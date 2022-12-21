0a0b7250510f31efa8e4533db7ed302ed6b8b5ed
semionc <x.sammythagi@gmail.com>
2022-12-21 16:53:29 +0700

    update

--- form-pendaftaran.php
+++ form-pendaftaran.php
@@ -9,42 +9,49 @@ include 'layout/head.php';
     <h1>Formulir Pendaftaran Murid TA 2022/2023</h1>
 
     <form action="" method="post">
-    <div class="row mt-4">
-        <div class="col-sm mt-3">
+    <div class="row mt-4" style="border: 1px solid grey;">
+        <div class="col-sm mt-3" style="padding: 30px 50px">
             <div class="mb-3">
-            <label for="nama" class="form-label">Nama Lengkap </label>
-            <input type="text" class="form-control" id="nama" name="nama" style="width: 55%" placeholder="NAMA PESERTA" required>
+                <label for="nama" class="form-label">Nama Lengkap </label>
+                <input type="text" class="form-control" id="nama" name="nama" style="width: 60%" placeholder="NAMA PESERTA" required>
             </div>
 
             <div class="mb-3">
-            <label for="nisn" class="form-label">NISN (Nomor Induk Siswa Nasional)</label>
-            <input type="number" class="form-control" id="nisn" name="nisn" style="width:55%" placeholder="NISN PESERTA"  required>
+                <label for="nisn" class="form-label">NISN (Nomor Induk Siswa Nasional)</label>
+                <input type="number" class="form-control" id="nisn" name="nisn" style="width:60%" placeholder="NISN PESERTA"  required>
             </div>
 
             <div class="mb-3">
-            <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-            <select class="form-select" id="jenis_kelamin" name="jenis_kelamin" style="width: 55%" required>
-                <option selected value="">--</option>
-                <option value="Perempuan">Perempuan</option>
-                <option value="Laki-laki">Laki-laki</option>
-            </select>
+                <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+                <select class="form-select" id="jenis_kelamin" name="jenis_kelamin" style="width: 60%" required>
+                    <option selected value="">::Pilih Jenis Kelamin::</option>
+                    <option value="Perempuan">Perempuan</option>
+                    <option value="Laki-laki">Laki-laki</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="tanggal_lahir">Tanggal Lahir</label>
+                <input class="form-control" id="tanggal_lahir" name="tanggal_lahir" type="date" style="width: 60%">
             </div>
         </div>
             
-        <div class="col-sm mt-3">
+        <div class="col-sm mt-3" style="padding: 30px 50px">
             <div class="mb-3">
                 <label for="alamat" class="form-label">Alamat Sesuai KK</label>
-                <textarea class="form-control" id="alamat" rows="3" style="width: 50%"></textarea>
+                <textarea class="form-control" id="alamat" rows="2" style="width: 60%" placeholder="ALAMAT"></textarea>
             </div>
 
             <div class="mb-3">
                 <label for="email" class="form-label">E-mail Orangtua (Ayah/Ibu)</label>
-                <input type="text" class="form-control" id="email" name="email" style="width: 50%" placeholder=""  required>
+                <input type="text" class="form-control" id="email" name="email" style="width: 60%; text-transform: none;" 
+                placeholder="contoh: abc@gmail.com"  required>
             </div>
 
             <div class="mb-3">
                 <label for="no_telp" class="form-label">No HP Aktif (No WA)</label>
-                <input type="text" class="form-control" id="no_telp" name="no_telp" style="width: 40%" placeholder=""  required>
+                <input type="text" class="form-control" id="no_telp" name="no_telp" style="width: 40%; text-transform: none;" 
+                placeholder="contoh: 081xxxxxxxxx"  required>
             </div>
 
         </div>
