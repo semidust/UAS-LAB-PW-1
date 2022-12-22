e8db65c7b10630ee7944f7e71a9f2473e15ce56f
rodia02 <117840720+rodia02@users.noreply.github.com>
2022-12-22 17:46:32 +0700

    update

--- home.php
+++ home.php
@@ -29,6 +29,7 @@ include 'layout/header.php';
         }
         
         .banner h1 {
+            margin: 75px 200px;
             color: rgb(6, 7, 7);
             z-index: 1;
             padding: 20px 25px;
@@ -162,7 +163,6 @@ include 'layout/header.php';
         .page .dot:hover {
             background-color: #ce0000;
         }
-
         section {
             margin: 75px 200px;
         }
@@ -178,15 +178,13 @@ include 'layout/header.php';
             word-spacing: 2px;
             line-height: 25px;
         }
-
        
     </style>
 </head>
 
 <body>
-  
     <section class="banner">
-    <center><h1>SD NEGERI 043950 SIGARANG - GARANG</h1></center>
+        <center><h1>SD NEGERI 043950 <br> SIGARANG - GARANG</h1></center>
     </section>
 
     <div class="container">
@@ -256,32 +254,15 @@ include 'layout/header.php';
             dot[slideIndex - 1].className += " active";
         }
     </script>
-
-    <!--Tentang Sekolah-->
     <section class="about">
         <div class ="container">
             <h2>Tentang Sekolah</h2>
             <p>SD NEGERI 043950 SIGARANG-GARANG adalah salah satu satuan pendidikan dengan jenjang SD di DESA SIGARANG-GARANG, Kec. Naman Teran, Kab. Karo, Sumatera Utara. Dalam menjalankan kegiatannya, SD NEGERI 043950 SIGARANG-GARANG berada di bawah naungan Kementerian Pendidikan dan Kebudayaan. Dengan akreditasi C, berdasarkan sertifikat 860/BANSM/PROVSU/LL/XII/2018.</p>
             <p>SD NEGERI 043950 SIGARANG-GARANG menyediakan listrik untuk membantu kegiatan belajar mengajar. Sumber listrik yang digunakan oleh SD NEGERI 043950 SIGARANG-GARANG berasal dari PLN. SD NEGERI 043950 SIGARANG-GARANG menyediakan akses internet yang dapat digunakan untuk mendukung kegiatan belajar mengajar menjadi lebih mudah. Provider yang digunakan SD NEGERI 043950 SIGARANG-GARANG untuk sambungan internetnya adalah Telkomsel Flash.</p>
-            <h2>VISI</h2>
-            <center><p>Unggul dalam bidang akademik dan non akademik, beriman, berbudaya dan berbudi pekerti luhur dalam perilaku.</p></center>
-            <h2>MISI</h2>
-            <p>1. Memacu siswa untuk berprestasi dalam bidang akademik dan non akademik minimal sampai tingkat kabupaten.</p>
-            <p>2. Mewujudkan prestasi sekolah di bidang akademik dan non akademik sampai pada tingkat nasional dan internasional.</p>
-            <p>3. Mewujudkan standar kelulusan dengan nilai minimal 75 sesuai dengan SNP.</p>
-            <p>4. Mewujudkan proses pembelajaran paikem, inovatif, dan kontekstual.</p>
-            <p>5. Mewujudkan sumber daya pendidik dan kependidikan yang profesional.</p>
-            <p>6. Meningkatkan kemampuan guru, pegawai, dan siswa dalam penggunaan bahasa asing (Bahasa Inggris).</p>
-            <p>7. Mewujudkan sarana dan prasarana belajar yang lengkap dan memadai sesuai dengan kebutuhan.</p>
-            <p>8. Mewujudkan manajemen sekolah yang transparan dan accuntabel.</p>
-            <p>9. Mewujudkan sistem penilaian yang berkualitas dan akurat.</p>
-            <p>10. Membiasakan warga sekolah untuk melaksanakan 5 S (Senyum, Salam, Sapa, Sopan dan Santun).</p>
-            <p>11. Menciptakan suasana kerja yang harmonis berdasarkan indikator budaya sekolah yaitu kedisiplinan, partisipasi, tanggung jawab, kebersamaan, kejujuran, kekeluargaan, semangat hidup dan semangat belajar.</p>
-            <p>12. Menanamkan sikap saling menghormati di antara warga sekolah.</p>
-            <p>13. Membiasakan peserta didik menjaga kebersihan lingkungan untuk mendukung program clean and green.</p>
-        
         </div>
     </section>
+
+    
 </body>
 
 </html>
---
+++ layout/visimisi.php
@@ -0,0 +1,76 @@
+<?php
+
+session_start();
+
+$title = 'SDN 043950';
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SDN 043950</title>
+    <style type="text/css">
+        * {
+            box-sizing: border-box;
+        }
+        
+        body {
+            font-family: Verdana, sans-serif;
+            margin: 0;
+            background-color: #bbb;
+        }
+        
+        .banner h1 {
+            color: rgb(6, 7, 7);
+            z-index: 1;
+            padding: 20px 25px;
+            border: 5px solid rgb(6, 7, 7);
+        }
+        section {
+            margin: 75px 200px;
+        }
+
+        section h2 {
+            text-align: center;
+            color: rgb(15, 15, 16);
+            margin-bottom: 20px;
+        }
+
+        .about p {
+            color:#080808;
+            word-spacing: 2px;
+            line-height: 25px;
+        }
+    </style>
+    </head>
+    <body>
+        <section class="banner">
+            <center><h1>SD NEGERI 043950 SIGARANG - GARANG</h1></center>
+        </section>
+
+        <section class="about">
+            <div class ="container">
+                <h2>VISI</h2>
+                <center><p>Unggul dalam bidang akademik dan non akademik, beriman, berbudaya dan berbudi pekerti luhur dalam perilaku.</p></center>
+                <h2>MISI</h2>
+                <p>1. Memacu siswa untuk berprestasi dalam bidang akademik dan non akademik minimal sampai tingkat kabupaten.</p>
+                <p>2. Mewujudkan prestasi sekolah di bidang akademik dan non akademik sampai pada tingkat nasional dan internasional.</p>
+                <p>3. Mewujudkan standar kelulusan dengan nilai minimal 75 sesuai dengan SNP.</p>
+                <p>4. Mewujudkan proses pembelajaran paikem, inovatif, dan kontekstual.</p>
+                <p>5. Mewujudkan sumber daya pendidik dan kependidikan yang profesional.</p>
+                <p>6. Meningkatkan kemampuan guru, pegawai, dan siswa dalam penggunaan bahasa asing (Bahasa Inggris).</p>
+                <p>7. Mewujudkan sarana dan prasarana belajar yang lengkap dan memadai sesuai dengan kebutuhan.</p>
+                <p>8. Mewujudkan manajemen sekolah yang transparan dan accuntabel.</p>
+                <p>9. Mewujudkan sistem penilaian yang berkualitas dan akurat.</p>
+                <p>10. Membiasakan warga sekolah untuk melaksanakan 5 S (Senyum, Salam, Sapa, Sopan dan Santun).</p>
+                <p>11. Menciptakan suasana kerja yang harmonis berdasarkan indikator budaya sekolah yaitu kedisiplinan, partisipasi, tanggung jawab, kebersamaan, kejujuran, kekeluargaan, semangat hidup dan semangat belajar.</p>
+                <p>12. Menanamkan sikap saling menghormati di antara warga sekolah.</p>
+                <p>13. Membiasakan peserta didik menjaga kebersihan lingkungan untuk mendukung program clean and green.</p>
+            </div>
+        </section>
+    </body>
+</html>
